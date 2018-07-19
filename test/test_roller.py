bf48033e0fbc9b1844e7a91679f49fcc92ac3ee6
kruegz <krueger.zach@gmail.com>
2018-07-18 22:32:40 -0500

    Roller unit test improvement

--- test/test_roller.py
+++ test/test_roller.py
@@ -3,7 +3,7 @@ from src.roller import Roller
 
 class TestRoller(unittest.TestCase):
 
-    def test_roller(self):
+    def test_roller1(self):
 
         roller = Roller("2d20+5")
 
@@ -11,4 +11,28 @@ class TestRoller(unittest.TestCase):
         self.assertEqual(roller.die, 20)
         self.assertEqual(roller.plus, 5)
 
+        r = roller.roll()
+        self.assertTrue(7 <= r <= 45)
+
+    def test_roller2(self):
+
+        roller = Roller("12+3")
+
+        self.assertEqual(roller.n, 1)
+        self.assertEqual(roller.die, 12)
+        self.assertEqual(roller.plus, 3)
+        
+        r = roller.roll()
+        self.assertTrue(4 <= r <= 15)
+    
+    def test_roller3(self):
+
+        roller = Roller("6")
+
+        self.assertEqual(roller.n, 1)
+        self.assertEqual(roller.die, 6)
+        self.assertEqual(roller.plus, 0)
+        
+        r = roller.roll()
+        self.assertTrue(1 <= r <= 6)
 
